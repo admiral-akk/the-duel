79b5c8d1c0d00be72921d1b7b14a7600eeb29076
Kuba Karpierz <mr.karpierz@gmail.com>
2024-01-12 13:39:22 -0500

    added basic networking between tabs

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import * as WEBRTC from "webrtc-adapter";
 import GUI from "lil-gui";
 import { gsap } from "gsap";
 import Stats from "stats-js";
@@ -281,6 +282,161 @@ const initLoadingAnimation = () => {
 };
 
 /**
+ * Networking
+ *
+ * Logic largely replicated from: https://webrtc.github.io/samples/
+ */
+let pc;
+let sendChannel;
+let receiveChannel;
+
+const signaling = new BroadcastChannel("webrtc");
+signaling.onmessage = (e) => {
+  switch (e.data.type) {
+    case "offer":
+      handleOffer(e.data);
+      break;
+    case "answer":
+      handleAnswer(e.data);
+      break;
+    case "candidate":
+      handleCandidate(e.data);
+      break;
+    case "ready":
+      // A second tab joined. This tab will enable the start button unless in a call already.
+      if (pc) {
+        console.log("already in call, ignoring");
+        return;
+      }
+      break;
+    case "bye":
+      if (pc) {
+        hangup();
+      }
+      break;
+    default:
+      console.log("unhandled", e);
+      break;
+  }
+};
+signaling.postMessage({ type: "ready" });
+
+const connect = async () => {
+  await createPeerConnection();
+  sendChannel = pc.createDataChannel("sendDataChannel");
+  sendChannel.onopen = onSendChannelStateChange;
+  sendChannel.onmessage = onSendChannelMessageCallback;
+  sendChannel.onclose = onSendChannelStateChange;
+
+  const offer = await pc.createOffer();
+  signaling.postMessage({ type: "offer", sdp: offer.sdp });
+  await pc.setLocalDescription(offer);
+};
+
+const close = async () => {
+  hangup();
+  signaling.postMessage({ type: "bye" });
+};
+
+async function hangup() {
+  if (pc) {
+    pc.close();
+    pc = null;
+  }
+  sendChannel = null;
+  receiveChannel = null;
+  console.log("Closed peer connections");
+}
+
+function createPeerConnection() {
+  pc = new RTCPeerConnection();
+  pc.onicecandidate = (e) => {
+    const message = {
+      type: "candidate",
+      candidate: null,
+    };
+    if (e.candidate) {
+      message.candidate = e.candidate.candidate;
+      message.sdpMid = e.candidate.sdpMid;
+      message.sdpMLineIndex = e.candidate.sdpMLineIndex;
+    }
+    signaling.postMessage(message);
+  };
+}
+
+async function handleOffer(offer) {
+  if (pc) {
+    console.error("existing peerconnection");
+    return;
+  }
+  await createPeerConnection();
+  pc.ondatachannel = receiveChannelCallback;
+  await pc.setRemoteDescription(offer);
+
+  const answer = await pc.createAnswer();
+  signaling.postMessage({ type: "answer", sdp: answer.sdp });
+  await pc.setLocalDescription(answer);
+}
+
+async function handleAnswer(answer) {
+  if (!pc) {
+    console.error("no peerconnection");
+    return;
+  }
+  await pc.setRemoteDescription(answer);
+}
+
+async function handleCandidate(candidate) {
+  if (!pc) {
+    console.error("no peerconnection");
+    return;
+  }
+  if (!candidate.candidate) {
+    await pc.addIceCandidate(null);
+  } else {
+    await pc.addIceCandidate(candidate);
+  }
+}
+
+let iter = 1;
+
+function sendData() {
+  iter++;
+  if (sendChannel) {
+    sendChannel.send(iter);
+  } else {
+    receiveChannel.send(iter);
+  }
+  console.log("Sent Data: " + iter);
+}
+
+function receiveChannelCallback(event) {
+  console.log("Receive Channel Callback");
+  receiveChannel = event.channel;
+  receiveChannel.onmessage = onReceiveChannelMessageCallback;
+  receiveChannel.onopen = onReceiveChannelStateChange;
+  receiveChannel.onclose = onReceiveChannelStateChange;
+}
+
+function onReceiveChannelMessageCallback(event) {
+  console.log("Received Message", event.data);
+}
+
+function onSendChannelMessageCallback(event) {
+  console.log("Received Message", event.data);
+}
+
+function onSendChannelStateChange() {
+  const readyState = sendChannel.readyState;
+  console.log("Send channel state is: " + readyState);
+}
+
+function onReceiveChannelStateChange() {
+  const readyState = receiveChannel.readyState;
+  console.log(`Receive channel state is: ${readyState}`);
+}
+
+/**
  * Loaded Objects
  */
 loadTexture("matcap01");
@@ -319,6 +475,7 @@ const tick = () => {
     timeTracker.elapsedTime =
       timeTracker.elapsedTime + debugObject.timeSpeed * clock.getDelta();
   }
+
   // update controls
   controls.update();
 
